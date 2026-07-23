19ab04ffc1143505f5ac07a1ca68e36cb7d85842
Murilo Scapim <mhscapim@gmail.com>
2026-07-23 20:19:14 -0300

    Aula 23/07

--- Crud_produtos_dicionario.py
+++ Crud_produtos_dicionario.py
@@ -32,3 +32,53 @@ while True:
                 print("\n --- Produtos no estoque ---")
                 for produto in produtos:
                     print(f"Nome: {produto["nome"]} | Preço: {produto["preco"]:.2f} | Quantidade: {produto["quantidade"]}")
+        case "3":
+            nome = input("Digite o nome do produto: ")
+
+            econtrado = False
+
+            for produto in produtos:
+                if produto["nome"].lower() == nome.lower():
+                    produto["nome"] = input("Digite o novo nome: ")
+                    produto["preco"] = float(input("Digite o novo preço: "))
+                    produto["quantidade"] = int(input("Digite a nova quantidade: "))
+                    print("Produto atualizado com sucesso!")
+
+                    encontrado = True
+                    break
+
+            if econtrado == False:
+                print("Produto não encontrado")
+        case "4":
+            nome = input("Digite o nome do produto: ")
+
+            econtrado = False
+
+            for produto in produtos:
+                if produto["nome"].lower() == nome.lower():
+                    produtos.remove(produto)
+                    print("Produto removido com sucesso!")
+                    encontrado = True
+                    break
+
+            if encontrado == False:
+                print("Produto não encontrado")
+        case "5":
+            nome = input("Digite o nome do produto: ")
+
+            for produto in produtos:
+                if produto["nome"].lower() == nome.lower():
+                    entrada = int(input("Digite a quantidade: "))
+                    produto["quantidade"] += entrada  # produto["quantidade"] =  produto["quantidade"] + entrada
+
+                    print("Estoque atualizado com sucesso!")
+                    encontrado = True
+                    break
+
+            if encontrado == False:
+                print("Produto não encontrado")
+        case "6":
+            print("Programa encerrado. Até logo!")
+            break
+        case _:
+            print("Opção inválida!")
